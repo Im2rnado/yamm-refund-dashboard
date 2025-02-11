e9714203be0d513a2ec0c60994b3cffe73073a89
Im2rnado <yassin.bedier@gmail.com>
2025-02-11 17:51:39 +0200

    create query provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
 import Sidebar from "./components/Sidebar";
+import Providers from './providers'
 import "../styles/globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="flex">
-        <Sidebar />
-        <main className="ml-64 w-full p-6">{children}</main>
+        <Providers>
+          <Sidebar />
+          <main className="ml-64 w-full p-6">{children}</main>
+        </Providers>
       </body>
     </html>
   );
